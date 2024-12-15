331e1a9d91cca9c408b0dc6fd62cccc2ee7d0242
s27549-pj <s27549@pjwstk.edu.pl>
2024-12-15 23:00:41 +0100

    Removing redundant imports

--- src/main/java/io/github/byttrio/andjo/flashcard/FlashcardController.java
+++ src/main/java/io/github/byttrio/andjo/flashcard/FlashcardController.java
@@ -1,8 +1,6 @@
 package io.github.byttrio.andjo.flashcard;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
