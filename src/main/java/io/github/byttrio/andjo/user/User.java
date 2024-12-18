6c648e8dff98406f137a2049ae51f638c12545c2
s27549-pj <s27549@pjwstk.edu.pl>
2024-12-18 18:55:18 +0100

    Updating user model

--- src/main/java/io/github/byttrio/andjo/user/User.java
+++ src/main/java/io/github/byttrio/andjo/user/User.java
@@ -1,17 +1,25 @@
 package io.github.byttrio.andjo.user;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private Integer totalPoints;
+    private LocalDateTime updatedAt = LocalDateTime.now();
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
