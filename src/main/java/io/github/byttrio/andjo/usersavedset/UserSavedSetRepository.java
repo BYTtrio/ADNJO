dd3789c009955692050e5289c09683f38e61eab4
s27549-pj <s27549@pjwstk.edu.pl>
2024-12-19 19:28:35 +0100

    This package is redundant, a join table is created in user model

--- src/main/java/io/github/byttrio/andjo/usersavedset/UserSavedSetRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.byttrio.andjo.usersavedset;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UserSavedSetRepository extends JpaRepository<UserSavedSet, UUID> {}
