ed8751cbdc7b1d672655ae1b3865240e12c1e2ef
s27549-pj <s27549@pjwstk.edu.pl>
2024-12-19 19:27:27 +0100

    Changing name to meet the naming conventions of both databases and java

--- src/main/java/io/github/byttrio/andjo/category/Category.java
+++ src/main/java/io/github/byttrio/andjo/category/Category.java
@@ -15,7 +15,8 @@ import java.util.UUID;
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID categoryId;
+    @Column(name = "category_id")
+    private UUID id;
 
     @Column(nullable = false, unique = true)
     private String name;
