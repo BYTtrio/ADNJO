bd0725e9653d51093a91d56a205e7c0db5f20efd
s27549 <s27549@pjwstk.edu.pl>
2025-01-12 10:00:36 +0100

    Adding profile page with user data

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,7 +1,36 @@
 import Navbar from "@/components/navbar";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import User from "@/interfaces/user";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
+  const [userData, setUserData] = useState<User>();
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/user/' + '44444444-4444-4444-4444-444444444444')
+      .then(response => response.json())
+      .then(json => setUserData(json))
+      .catch(error => console.error(error));
+  }, []);
+
+  const user = (
+    <Card className='user' key={userData?.id}>
+      <CardTitle>
+      <p><b>Username: </b>{userData?.username}</p>
+      </CardTitle>
+      <CardContent>
+        <p><b>Email: </b>{userData?.email}</p>
+        <p><b>Total points: </b>{userData?.totalPoints}</p>
+        <p><b>Account last updated at: </b>{userData?.updatedAt}</p>
+        <p><b>Account created at: </b>{userData?.createdAt}</p>
+      </CardContent>
+    </Card>
+  )
+
   return (
-    <Navbar />
+    <>
+      <Navbar /> 
+      {user}
+    </>
   )
 }
