86e21fbd5cd4f45f3205dc5196c1cfd6fa4eeba1
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-01-18 05:38:23 +0100

    leaderboard renamed to leaderboard-page

--- frontend/src/pages/leaderboard-page.tsx
+++ frontend/src/pages/leaderboard-page.tsx
@@ -1,10 +1,11 @@
+import Leaderboard from "@/components/leaderboard";
 import Navbar from "@/components/navbar";
 
 export default function LeaderboardPage() {
   return (
     <>
       <Navbar />
-      <div className="table"></div>
+      <Leaderboard />
     </>
   )
 }
