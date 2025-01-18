98ad0e85e894b8f7674c73bbc6a056abfc7acb71
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-01-18 05:37:46 +0100

    Removing theme toggle from login and register page

--- frontend/src/pages/login-page.tsx
+++ frontend/src/pages/login-page.tsx
@@ -5,7 +5,6 @@ export default function LoginPage() {
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
-        <ModeToggle/>
       </div>
     </div>
   )
