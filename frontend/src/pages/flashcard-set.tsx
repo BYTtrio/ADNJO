f76c1045b4e9edf11368c9f7617948d51e0ca8ce
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-01-19 08:06:36 +0100

    Added flashcard set review page

---
+++ frontend/src/pages/flashcard-set.tsx
@@ -0,0 +1,48 @@
+import Navbar from "@/components/navbar";
+import Flashcard from "@/interfaces/flashcard";
+import { useEffect, useState } from "react";
+import { FlashcardArray } from "react-quizlet-flashcard";
+import { useParams } from "react-router-dom";
+
+export default function FlashcardSetPage() {
+  const [flashcardData, setFlashcardData] = useState<Flashcard[]>([])
+  const {id} = useParams();
+  useEffect(() => {
+      fetch('http://localhost:8080/api/flashcards/set/' + id)
+      .then(response => response.json())
+      .then(json => setFlashcardData(json))
+      .catch(error => console.error(error));
+      console.log(flashcardData);
+  }, []);
+
+  const cards = flashcardData.map( flashcard => ({
+      id: flashcard.id,
+      frontHTML: flashcard.term,
+      backHTML: flashcard.definition,
+  }));
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center">
+        <FlashcardArray
+          cards = {cards}
+          cycle = {true}
+          frontContentStyle={{
+            backgroundColor: "lightgoldenrodyellow",
+            color: "black",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          backContentStyle={{
+            backgroundColor: 'hsl(var(--foreground))',
+            color: 'hsl(var(--primary))',
+            justifyContent: "center",
+            alignItems: "center",
+            border: '--border',
+          }}
+        />
+      </div>
+    </>
+  )
+}
