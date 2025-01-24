b1fe98f5e662c3d1115748ce497d56ad122e2eed
Aleksander Stankowski <s27549@pjwstk.edu.pl>
2025-01-24 08:52:57 +0100

    Adjusting carousel size

--- frontend/src/pages/flashcard-set.tsx
+++ frontend/src/pages/flashcard-set.tsx
@@ -19,6 +19,7 @@ export default function FlashcardSetPage() {
       id: flashcard.id,
       frontHTML: flashcard.term,
       backHTML: flashcard.definition,
+      width: '95%'
   }));
 
   return (
